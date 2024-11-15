e8d184d7f9f1196a9553bb536eaf420b6d0c27ad
Hong Sheng Tan <skytanhongsheng@gmail.com>
2024-11-15 15:34:05 +0800

    Watch List Completed Except Design

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,29 @@
+class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list_id = params[:list_id]
+    if @bookmark.save
+      redirect_to list_path(params[:list_id])
+    else
+      @list = List.find(params[:list_id])
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list_id)
+  end
+
+private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
